7857170f27c2770db018b1d39091fe6ca9105526
Hieu Nguyen <duchieu1191103@gmail.com>
2023-07-28 20:19:53 +0700

    add relation for entity Order

--- src/Entity/Order.php
+++ src/Entity/Order.php
@@ -22,10 +22,17 @@ class Order
     #[ORM\ManyToMany(targetEntity: self::class, mappedBy: 'category')]
     private Collection $Orders;
 
+    #[ORM\ManyToOne(targetEntity: self::class, inversedBy: 'user')]
+    private ?self $users = null;
+
+    #[ORM\OneToMany(mappedBy: 'users', targetEntity: self::class)]
+    private Collection $user;
+
     public function __construct()
     {
         $this->category = new ArrayCollection();
         $this->Orders = new ArrayCollection();
+        $this->user = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -79,6 +86,48 @@ class Order
     {
         if ($this->Orders->removeElement($order)) {
             $order->removeCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function getUsers(): ?self
+    {
+        return $this->users;
+    }
+
+    public function setUsers(?self $users): static
+    {
+        $this->users = $users;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, self>
+     */
+    public function getUser(): Collection
+    {
+        return $this->user;
+    }
+
+    public function addUser(self $user): static
+    {
+        if (!$this->user->contains($user)) {
+            $this->user->add($user);
+            $user->setUsers($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUser(self $user): static
+    {
+        if ($this->user->removeElement($user)) {
+            // set the owning side to null (unless already changed)
+            if ($user->getUsers() === $this) {
+                $user->setUsers(null);
+            }
         }
 
         return $this;
