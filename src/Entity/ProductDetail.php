4fe947ffe9c81f69c1bdf5c265819ef0e991e1ac
Hieu Nguyen <duchieu1191103@gmail.com>
2023-07-28 20:45:05 +0700

    add product detail

---
+++ src/Entity/ProductDetail.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductDetailRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductDetailRepository::class)]
+class ProductDetail
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\OneToOne(cascade: ['persist', 'remove'])]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Product $Product = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProduct(): ?Product
+    {
+        return $this->Product;
+    }
+
+    public function setProduct(Product $Product): static
+    {
+        $this->Product = $Product;
+
+        return $this;
+    }
+}
